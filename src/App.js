967f492f6397c50863b5da613395936a0234ace3
Seno Agung Pradityo <senoagung27@gmail.com>
2022-06-20 09:55:34 +0700

    bug error

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import LandingPage from "pages/LandingPage";
 
 import "assets/scss/style.scss";
 
+
 function App() {
-  return (
-    <div className="App">
-      hallo World
-    </div>
-  );
+  return <div className="App">
+    <Router>
+        <Route exact={true} path="/" element={<LandingPage/>} />
+</Router>
+      {/* <Router>
+          <Route exact path="/" component={<LandingPage/>} />
+      </Router> */}
+    </div>;
 }
 
 export default App;
--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,15 +1,15 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-import second from "elements/Button";
+import Header from "parts/Header";
 
-class LandingPage extends Component {
+export default class LandingPage extends Component {
     render() {
+        console.log(this.props);
         return (
-            <div>
-                
-            </div>
+            <>
+            <Header {...this.props}></Header>
+            </>
         );
     }
 }
 
-export default LandingPage;
